1585155a12e78ea62eff190b0e99e529fe1d4d87
Ragav Kumar <ragavkumarv@gmail.com>
2021-03-07 11:36:21 +0530

    adding route

--- index.js
+++ index.js
@@ -11,24 +11,32 @@ const app = Express();
 const port = 3200;
 
 app.use(cors());
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
 
 app.get("/", (request, response) => {
   response.send("Hello world4");
 });
 
-app.get("/recipes", (request, response) => {
-  console.log(request.query);
+app
+  .route("/recipes")
+  .get((request, response) => {
+    console.log(request.query);
 
-  const queryMatch = request.query.ings
-    ? recipes.filter(
-        (recipe) =>
-          request.query.like === `${recipe.like}` &&
-          recipe.ings.includes(request.query.ings)
-      )
-    : recipes;
-  console.log(queryMatch);
-  response.send(queryMatch);
-});
+    const queryMatch = request.query.ings
+      ? recipes.filter(
+          (recipe) =>
+            request.query.like === `${recipe.like}` &&
+            recipe.ings.includes(request.query.ings)
+        )
+      : recipes;
+    console.log(queryMatch);
+    response.send(queryMatch);
+  })
+  .post((request, response) => {
+    console.log(request.body);
+    response.send([...recipes, request.body]);
+  });
 
 app.get("/recipes/:id", (request, response) => {
   console.log(request.params.id);
@@ -44,5 +52,7 @@ app.get("/recipes/:id", (request, response) => {
   response.send(searchRecipe);
   // response.send(searchFilterRecipe);
 });
+
+// CRUD -> Create Read Update Delete
 
 app.listen(port, () => console.log("Started"));
